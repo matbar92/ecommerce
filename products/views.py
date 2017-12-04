37922fbbb6139345cb0190e2525273155eee8ddb
Mateusz Baranowski <mateuszbaranowski@MacBook-Pro-Mateusz.local>
2017-12-04 22:01:03 +0100

    staticfiles

---
+++ products/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template.response import TemplateResponse
+# Create your views here.
+from .models import Product
+
+
+class HomeView(View):
+
+    def get(self, request):
+        products = Product.objects.all()
+        template = 'products/home.html'
+        ctx = {'products': products}
+        return render(request,template,ctx)
+
+class ProductsView(View):
+
+    def get(self,request):
+        template = 'products/all.html'
+        products = Product.objects.all()
+        ctx = {'products': products}
+        return render(request, template, ctx)
